3dcb350be83fff337182c82facf81f619b8ccf86
Laurent Peuch <cortex@worlddomination.be>
2019-01-04 01:03:22 +0100

    [mod] only connect once to XMPP bot

--- latest_posts.py
+++ latest_posts.py
@@ -15,33 +15,33 @@ def main(password):
     else:
         db = {"post_ids": []}
 
-    raw_categories = json.load(urlopen("https://forum.yunohost.org/site.json"))["categories"]
-    categories = {x["id"]: x["name"] for x in raw_categories}
+    with XMPPBot(password, room="forum") as bot:
+        raw_categories = json.load(urlopen("https://forum.yunohost.org/site.json"))["categories"]
+        categories = {x["id"]: x["name"] for x in raw_categories}
 
-    for category in raw_categories:
-        if "parent_category_id" in category:
-            categories[category["id"]] = categories[category["parent_category_id"]] + "/" + category["name"]
+        for category in raw_categories:
+            if "parent_category_id" in category:
+                categories[category["id"]] = categories[category["parent_category_id"]] + "/" + category["name"]
 
-    for post in reversed(json.load(urlopen("https://forum.yunohost.org/posts.json"))["latest_posts"]):
-        if post["id"] in db["post_ids"]:
-            continue
-        else:
-            db["post_ids"].append(post["id"])
+        for post in reversed(json.load(urlopen("https://forum.yunohost.org/posts.json"))["latest_posts"]):
+            if post["id"] in db["post_ids"]:
+                continue
+            else:
+                db["post_ids"].append(post["id"])
 
-        category = categories[post["category_id"]]
-        title = post["topic_title"]
-        user = post["username"]
-        url = "https://forum.yunohost.org/t/%s/%s/%s" % (post["topic_slug"], post["topic_id"], post["post_number"])
+            category = categories[post["category_id"]]
+            title = post["topic_title"]
+            user = post["username"]
+            url = "https://forum.yunohost.org/t/%s/%s/%s" % (post["topic_slug"], post["topic_id"], post["post_number"])
 
-        extract = post["raw"].replace("\n", " ")
-        if len(extract) > 200:
-            extract = extract[:200] + "..."
+            extract = post["raw"].replace("\n", " ")
+            if len(extract) > 200:
+                extract = extract[:200] + "..."
 
-        to_send = u'[%s] « %s » @%s: %s %s' % (category, title, user, extract, url)
+            to_send = u'[%s] « %s » @%s: %s %s' % (category, title, user, extract, url)
 
-        print(to_send)
+            print(to_send)
 
-        with XMPPBot(password, room="forum") as bot:
             bot.sendToChatRoom(to_send)
             time.sleep(3)
 
